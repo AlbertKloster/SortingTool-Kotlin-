a6592010db8b33165aca0dd280042644f67767f7
AlbertKloster <albert.kloster@gmail.com>
2023-06-26 21:23:18 -0400

    Stage1

--- Main.kt
+++ Main.kt
@@ -1,5 +1,16 @@
 package sorting
 
+import java.util.Scanner
+
 fun main() {
-    // write your code here
+    val numbers = mutableListOf<Int>()
+    val scanner = Scanner(System.`in`)
+
+    while (scanner.hasNext()) {
+        numbers.addAll(scanner.next().split(Regex("\\s+")).map { it.toInt() })
+    }
+
+    println("Total numbers: ${numbers.size}.")
+    println("The greatest number: ${numbers.maxOf { it }} (${numbers.count { it == numbers.maxOf { it } }} time(s)).")
+
 }
